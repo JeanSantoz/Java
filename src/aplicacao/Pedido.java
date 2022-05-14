d9c73a0783625ef0c23839b11839778b02295aa5
Jean Ferreira <Jeansantoz13@gmail.com>
2022-05-14 17:56:03 -0300

    Classe Pedido Finalizada

--- src/aplicacao/Pedido.java
+++ src/aplicacao/Pedido.java
@@ -1,5 +1,71 @@
 package aplicacao;
 
+import java.util.Date;
+import enums.OrderStatus;
+
 public class Pedido {
+	
+	// Atributos
+	
+	private Integer id;
+	private Date momento;
+	private OrderStatus status;
+	
+	// Construtores
+	
+	public Pedido() {};
+	
+	public Pedido(Integer id, Date momento, OrderStatus status) {
+		
+		this.id = id;
+		this.momento = momento;
+		this.status = status;
+		
+	};
+	
+	// gets and sets
+	
+	public void setId(Integer id) {
+		
+		this.id = id;
+		
+	}
+	
+	public Integer getId() {
+			
+		return id;
+			
+	}
+	
+	public void setMomento(Date momento) {
+		
+		this.momento = momento;
+		
+	}
+	
+	public Date getMomento() {
+			
+		return momento;
+			
+	}
+	
+	public void setOrderStatus(OrderStatus status) {
+		
+		this.status = status;
+		
+	}
+	
+	public OrderStatus getOrderStatus() {
+			
+		return status;
+			
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", momento=" + momento + ", status=" + status + "]";
+	}
+	
+	
 
 }
--- src/aplicacao/Program.java
+++ src/aplicacao/Program.java
@@ -1,9 +1,22 @@
 package aplicacao;
 
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Pedido pedido = new Pedido(001,new Date(), OrderStatus.PENDENTE);
+		
+		sc.close();
 
 	}
 
